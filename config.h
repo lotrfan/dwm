72600ee3aa728e83ab531f6165db3616b50d6194
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2013-09-01 00:23:30 -0500

    By default, don't show bottom bar

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
-static const Bool showbbar          = True;     /* False means no bar */
+static const Bool showbbar          = False;    /* False means no bar */
 
 /* False means using the scroll wheel on a window will not change focus */
 static const Bool focusonwheelscroll = False;
