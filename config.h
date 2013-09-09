51e1d746e3f706b679ea35f5b163e1db2ab1ea16
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2013-09-08 20:45:38 -0500

    Fix alignment

--- config.h
+++ config.h
@@ -97,78 +97,78 @@ void self_restart(const Arg *arg) {
 #include <X11/XF86keysym.h>
 
 static Key keys[] = {
-	/* modifier                               key              function        argument */
-	{ MODKEY,                                 XK_space,        spawn,          {.v = dmenucmd } },
-	{ MODKEY,                                 XK_a,            spawn,          {.v = passcmd } },
-	{ MODKEY,                                 XK_c,            spawn,          {.v = calccmd } },
-	{ MODKEY,                                 XK_s,            spawn,          {.v = srcmd } },
+	/* modifier                      , key                     , function       , argument */
+	{ MODKEY                         , XK_space                , spawn          , {.v = dmenucmd } }           ,
+	{ MODKEY                         , XK_a                    , spawn          , {.v = passcmd } }            ,
+	{ MODKEY                         , XK_c                    , spawn          , {.v = calccmd } }            ,
+	{ MODKEY                         , XK_s                    , spawn          , {.v = srcmd } }              ,
 
-	{ MODKEY|ShiftMask,                       XK_Return,       spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask               , XK_Return               , spawn          , {.v = termcmd } }            ,
 
 	/* Find keys with xev */
-	{ 0,                                      XF86XK_AudioRaiseVolume,      spawn,          {.v = volupcmd } },
-	{ 0,                                      XF86XK_AudioLowerVolume,      spawn,          {.v = voldowncmd } },
-	{ 0,                                      XF86XK_AudioMute,      spawn,          {.v = volmutecmd } },
+	{ 0                              , XF86XK_AudioRaiseVolume , spawn          , {.v = volupcmd } }           ,
+	{ 0                              , XF86XK_AudioLowerVolume , spawn          , {.v = voldowncmd } }         ,
+	{ 0                              , XF86XK_AudioMute        , spawn          , {.v = volmutecmd } }         ,
 
-	{ 0,                                      XF86XK_AudioPlay,      spawn,          {.v = music_playpausecmd } },
-	//{ 0,                                      0x1008ff31,      spawn,          {.v = music_playpausecmd } },
-	{ 0,                                      XF86XK_AudioPrev,      spawn,          {.v = music_prevcmd } },
-	{ 0,                                      XF86XK_AudioNext,      spawn,          {.v = music_nextcmd } },
+	{ 0                              , XF86XK_AudioPlay        , spawn          , {.v = music_playpausecmd } } ,
+	//{ 0                            , 0x1008ff31              , spawn          , {.v = music_playpausecmd } } ,
+	{ 0                              , XF86XK_AudioPrev        , spawn          , {.v = music_prevcmd } }      ,
+	{ 0                              , XF86XK_AudioNext        , spawn          , {.v = music_nextcmd } }      ,
 
-	{ 0,                                      XF86XK_TouchpadToggle,      spawn,          {.v = touchtogglecmd } },
+	{ 0                              , XF86XK_TouchpadToggle   , spawn          , {.v = touchtogglecmd } }     ,
 
-	{ 0,                                      XF86XK_Calculator,      spawn,          {.v = autobrightnesscmd } }, // calculator key
-	{ ControlMask,                            XF86XK_Calculator,      spawn,          {.v = redshifttogglecmd } }, // ctrl+calculator key
-	{ Mod4Mask,                               XK_Down,         spawn,          {.v = brightnessdowncmd } },
-	{ Mod4Mask,                               XK_Up,           spawn,          {.v = brightnessupcmd } },
+	{ 0                              , XF86XK_Calculator       , spawn          , {.v = autobrightnesscmd } }  , // calculator key
+	{ ControlMask                    , XF86XK_Calculator       , spawn          , {.v = redshifttogglecmd } }  , // ctrl+calculator key
+	{ Mod4Mask                       , XK_Down                 , spawn          , {.v = brightnessdowncmd } }  ,
+	{ Mod4Mask                       , XK_Up                   , spawn          , {.v = brightnessupcmd } }    ,
 
-	{ MODKEY,                                 XK_p,            spawn,          {.v = randrcmd } },
-	{ MODKEY,                                 XK_o,            spawn,          {.v = irandrcmd } },
-	{ Mod4Mask,                               XK_KP_Add,       spawn,          {.v = nextmodecmd } },
-	{ Mod4Mask,                               XK_KP_Subtract,  spawn,          {.v = prevmodecmd } },
-	{ Mod4Mask,                               XK_KP_Multiply,  spawn,          {.v = defaultmodecmd } },
+	{ MODKEY                         , XK_p                    , spawn          , {.v = randrcmd } }           ,
+	{ MODKEY                         , XK_o                    , spawn          , {.v = irandrcmd } }          ,
+	{ Mod4Mask                       , XK_KP_Add               , spawn          , {.v = nextmodecmd } }        ,
+	{ Mod4Mask                       , XK_KP_Subtract          , spawn          , {.v = prevmodecmd } }        ,
+	{ Mod4Mask                       , XK_KP_Multiply          , spawn          , {.v = defaultmodecmd } }     ,
 
-	{ Mod1Mask|ControlMask,                   XK_l,            spawn,          {.v = locknowcmd } },
-	{ Mod1Mask|ControlMask|ShiftMask,         XK_l,            spawn,          {.v = locktogglecmd } },
+	{ Mod1Mask|ControlMask           , XK_l                    , spawn          , {.v = locknowcmd } }         ,
+	{ Mod1Mask|ControlMask|ShiftMask , XK_l                    , spawn          , {.v = locktogglecmd } }      ,
 
-	{ MODKEY,                                 XK_b,            togglebar,      {.i = 0} },
-	{ MODKEY,                                 XK_v,            togglebar,      {.i = 1} },
-	{ MODKEY,                                 XK_j,            focusstack,     {.i = +1 } },
-	{ Mod1Mask,                               XK_Tab,          focusstack,     {.i = +1} },
-	{ MODKEY,                                 XK_k,            focusstack,     {.i = -1 } },
-	{ Mod1Mask|ShiftMask,                     XK_Tab,          focusstack,     {.i = -1} },
-	{ MODKEY|ControlMask,                     XK_j,            pushdown,       {0} },
-	{ MODKEY|ControlMask,                     XK_k,            pushup,         {0} },
-	{ MODKEY,                                 XK_i,            incnmaster,     {.i = +1 } },
-	{ MODKEY,                                 XK_d,            incnmaster,     {.i = -1 } },
-	{ MODKEY,                                 XK_h,            setmfact,       {.f = -0.05} },
-	{ MODKEY,                                 XK_l,            setmfact,       {.f = +0.05} },
-	{ MODKEY,                                 XK_Return,       zoom,           {0} },
-	{ MODKEY,                                 XK_Tab,          view,           {0} },
-	{ MODKEY|ShiftMask,                       XK_c,            killclient,     {0} },
-	{ MODKEY,                                 XK_t,            setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                                 XK_f,            setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                                 XK_m,            setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                                 XK_r,            setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                                 XK_e,            setlayout,      {.v = &layouts[4]} },
-	{ MODKEY|ShiftMask,                       XK_space,        togglefloating, {0} },
-	{ MODKEY,                                 XK_0,            view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,                       XK_0,            tag,            {.ui = ~0 } },
-	{ MODKEY,                                 XK_comma,        focusmon,       {.i = -1 } },
-	{ MODKEY,                                 XK_period,       focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,                       XK_comma,        tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,                       XK_period,       tagmon,         {.i = +1 } },
-	TAGKEYS(                                  XK_1,                      0)
-	TAGKEYS(                                  XK_2,                      1)
-	TAGKEYS(                                  XK_3,                      2)
-	TAGKEYS(                                  XK_4,                      3)
-	TAGKEYS(                                  XK_5,                      4)
-	TAGKEYS(                                  XK_6,                      5)
-	TAGKEYS(                                  XK_7,                      6)
-	TAGKEYS(                                  XK_8,                      7)
-	TAGKEYS(                                  XK_9,                      8)
-	{ MODKEY|ControlMask|ShiftMask,           XK_q,            quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask,           XK_r,            self_restart,           {0} },
+	{ MODKEY                         , XK_b                    , togglebar      , {.i = 0} }                   ,
+	{ MODKEY                         , XK_v                    , togglebar      , {.i = 1} }                   ,
+	{ MODKEY                         , XK_j                    , focusstack     , {.i = +1 } }                 ,
+	{ Mod1Mask                       , XK_Tab                  , focusstack     , {.i = +1} }                  ,
+	{ MODKEY                         , XK_k                    , focusstack     , {.i = -1 } }                 ,
+	{ Mod1Mask|ShiftMask             , XK_Tab                  , focusstack     , {.i = -1} }                  ,
+	{ MODKEY|ControlMask             , XK_j                    , pushdown       , {0} }                        ,
+	{ MODKEY|ControlMask             , XK_k                    , pushup         , {0} }                        ,
+	{ MODKEY                         , XK_i                    , incnmaster     , {.i = +1 } }                 ,
+	{ MODKEY                         , XK_d                    , incnmaster     , {.i = -1 } }                 ,
+	{ MODKEY                         , XK_h                    , setmfact       , {.f = -0.05} }               ,
+	{ MODKEY                         , XK_l                    , setmfact       , {.f = +0.05} }               ,
+	{ MODKEY                         , XK_Return               , zoom           , {0} }                        ,
+	{ MODKEY                         , XK_Tab                  , view           , {0} }                        ,
+	{ MODKEY|ShiftMask               , XK_c                    , killclient     , {0} }                        ,
+	{ MODKEY                         , XK_t                    , setlayout      , {.v = &layouts[0]} }         ,
+	{ MODKEY                         , XK_f                    , setlayout      , {.v = &layouts[1]} }         ,
+	{ MODKEY                         , XK_m                    , setlayout      , {.v = &layouts[2]} }         ,
+	{ MODKEY                         , XK_r                    , setlayout      , {.v = &layouts[3]} }         ,
+	{ MODKEY                         , XK_e                    , setlayout      , {.v = &layouts[4]} }         ,
+	{ MODKEY|ShiftMask               , XK_space                , togglefloating , {0} }                        ,
+	{ MODKEY                         , XK_0                    , view           , {.ui = ~0 } }                ,
+	{ MODKEY|ShiftMask               , XK_0                    , tag            , {.ui = ~0 } }                ,
+	{ MODKEY                         , XK_comma                , focusmon       , {.i = -1 } }                 ,
+	{ MODKEY                         , XK_period               , focusmon       , {.i = +1 } }                 ,
+	{ MODKEY|ShiftMask               , XK_comma                , tagmon         , {.i = -1 } }                 ,
+	{ MODKEY|ShiftMask               , XK_period               , tagmon         , {.i = +1 } }                 ,
+	{ MODKEY|ControlMask|ShiftMask   , XK_q                    , quit           , {0} }                        ,
+	{ MODKEY|ControlMask|ShiftMask   , XK_r                    , self_restart   , {0} }                        ,
+	TAGKEYS(  /*                     , */ XK_1                 , 0)
+	TAGKEYS(  /*                     , */ XK_2                 , 1)
+	TAGKEYS(  /*                     , */ XK_3                 , 2)
+	TAGKEYS(  /*                     , */ XK_4                 , 3)
+	TAGKEYS(  /*                     , */ XK_5                 , 4)
+	TAGKEYS(  /*                     , */ XK_6                 , 5)
+	TAGKEYS(  /*                     , */ XK_7                 , 6)
+	TAGKEYS(  /*                     , */ XK_8                 , 7)
+	TAGKEYS(  /*                     , */ XK_9                 , 8)
 };
 
 /* button definitions */
